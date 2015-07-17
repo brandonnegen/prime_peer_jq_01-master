5a607d53f182263fbf415f4780125a8f0a23f15e
brandonnegen <bnegen@gmail.com>
2015-07-17 07:56:23 -0500

    Fixed random first name

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 
 	$("#alertButton").on('click', function(event){
 		//make it alert randomAge + randomSex + randomWeight
-	$("#button").children().remove();
-	$("#button").prepend('<p>' + randomName + ' ' + randomLastName + ' is ' + randomAge + ' years old.</p>');
+	$("#alertButton").children().remove();
+	$("#anotherButton").append('<p>' + randomName + ' ' + randomLastName + ' is ' + randomAge + ' years old.</p>');
 	$("#anotherButton").append('<p> They are also a ' + randomSex + '.</p>');
 	$("#judgeyButton").append('<p>And they weigh a SHOCKING ' + randomWeight + ' lbs.</p>');
 	});
@@ -25,7 +25,7 @@ function randomNumber(min, max) {
 var randomAge = randomNumber(1, 100);
 var randomSex = randomNumber(0, 1);
 var randomWeight = randomNumber(1, 100);
-var randomName = randomNumber(0, 1);
+var randomName = randomNumber(0, 100);
 var femaleArray = ["Tina", "Sara", "Allison", "Sapphire", "Candy"];
 var maleArray = ["Bert", "Ted", "Jerome", "Warren", "Steve"];
 var lastNameArray = ["Sheckler", "Chipperson", "Hughes", "Bennet", "Fergurson"];
@@ -37,10 +37,9 @@ if (randomSex == 0){
 	randomSex = "Female";
 }
 
-if (randomName == 0 && randomSex == 0){
+if (randomSex == "Male"){
 	randomName = maleArray[Math.floor(Math.random() * maleArray.length)];
 } else {
-	randomName == 1 && randomSex == 1;
 	randomName = femaleArray[Math.floor(Math.random() * femaleArray.length)];
 
 }
