555ce3eba91109de4e4c833db4d7a650d21cb264
brandonnegen <bnegen@gmail.com>
2015-07-17 09:01:12 -0500

    Final Code

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
 
 	$("#alertButton").on('click', function(event){
 		//make it alert randomAge + randomSex + randomWeight
-	$("#alertButton").children().remove();
-	$("#anotherButton").append('<p>' + randomName + ' ' + randomLastName + ' is ' + randomAge + ' years old.</p>');
-	$("#anotherButton").append('<p> They are also a ' + randomSex + '.</p>');
-	$("#judgeyButton").append('<p>And they weigh a SHOCKING ' + randomWeight + ' lbs.</p>');
+		$("#anotherButton").children().remove();
+		var myPerson = personFactory();
+		$("#anotherButton").prepend('<p>And they weigh a SHOCKING ' + myPerson.randomWeight + ' lbs.</p>');
+		$("#anotherButton").prepend('<p> They are also a ' + myPerson.randomSex + '.</p>');
+		$("#anotherButton").prepend('<p>' + myPerson.randomName + ' ' + myPerson.randomLastName + ' is ' + myPerson.randomAge + ' years old.</p>');
 	});
 });
 function Person(randomAge, randomSex, randomWeight, randomName, randomLastName){
@@ -22,29 +23,34 @@ function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
-var randomAge = randomNumber(1, 100);
-var randomSex = randomNumber(0, 1);
-var randomWeight = randomNumber(1, 100);
-var randomName = randomNumber(0, 100);
-var femaleArray = ["Tina", "Sara", "Allison", "Sapphire", "Candy"];
-var maleArray = ["Bert", "Ted", "Jerome", "Warren", "Steve"];
-var lastNameArray = ["Sheckler", "Chipperson", "Hughes", "Bennet", "Fergurson"];
+function personFactory (argument) {
+	// body...
+	var randomAge = randomNumber(1, 100);
+	var randomSex = randomNumber(0, 1);
+	var randomWeight = randomNumber(1, 100);
+	var randomName = randomNumber(0, 100);
+	var femaleArray = ["Tina", "Sara", "Allison", "Sapphire", "Candy"];
+	var maleArray = ["Bert", "Ted", "Jerome", "Warren", "Steve"];
+	var lastNameArray = ["Sheckler", "Chipperson", "Hughes", "Bennet", "Fergurson"];
+	var randomLastName = lastNameArray[Math.floor(Math.random() * lastNameArray.length)];
+	if (randomSex == 0){
+		randomSex = "Male";
+	} else {
+		randomSex = "Female";
+	}
 
+	if (randomSex == "Male"){
+		randomName = maleArray[Math.floor(Math.random() * maleArray.length)];
+	} else {
+		randomName = femaleArray[Math.floor(Math.random() * femaleArray.length)];
 
-if (randomSex == 0){
-	randomSex = "Male";
-} else {
-	randomSex = "Female";
+	}
+	return new Person(randomAge, randomSex, randomWeight, randomName, randomLastName);
 }
 
-if (randomSex == "Male"){
-	randomName = maleArray[Math.floor(Math.random() * maleArray.length)];
-} else {
-	randomName = femaleArray[Math.floor(Math.random() * femaleArray.length)];
 
-}
 
-var randomLastName = lastNameArray[Math.floor(Math.random() * lastNameArray.length)];
+
 
 
 
